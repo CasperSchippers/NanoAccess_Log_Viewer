10426e7c18e17258f0b0346a2fce5c740b0d7fe4
Casper Schippers <c.f.schippers@tue.nl>
2019-08-30 12:45:07 +0200

    Added fileview

--- NanoAccess_Log_Viewer/main.py
+++ NanoAccess_Log_Viewer/main.py
@@ -30,6 +30,13 @@ class Viewer(QtWidgets.QMainWindow):
         open_file.setShortcut("Ctrl+O")
         open_file.triggered.connect(self.openFiles)
 
+        open_dir = file_menu.addAction("&Open Dir")
+        open_dir.setStatusTip("Select and open directory")
+        open_dir.setIcon(self.style().standardIcon(
+            QtWidgets.QStyle.SP_DirOpenIcon))
+        open_dir.setShortcut("Ctrl+D")
+        open_dir.triggered.connect(self.selectDir)
+
     def initStatusBar(self):
         self.statusbar = self.statusBar()
         self.statusbar.showMessage("Ready")
@@ -39,6 +46,12 @@ class Viewer(QtWidgets.QMainWindow):
         self.setCentralWidget(central_widget)
 
         central_layout = QtWidgets.QGridLayout(central_widget)
+
+        self.file_system = QtWidgets.QFileSystemModel()
+        self.file_system.setRootPath('D:/Git/')
+        self.file_tree = QtWidgets.QTreeView()
+        self.file_tree.setModel(self.file_system)
+        central_layout.addWidget(self.file_tree, 0, 0)
 
         self.plot_widget = pg.PlotWidget()
         plotItem = self.plot_widget.getPlotItem()
@@ -52,6 +65,11 @@ class Viewer(QtWidgets.QMainWindow):
     def selectFiles(self):
         self.filenames, _ = QtWidgets.QFileDialog.getOpenFileNames(
             self, 'Open File')
+
+    def selectDir(self):
+        directory = QtWidgets.QFileDialog.getExistingDirectory(
+            self, 'Select Directory')
+        self.file_tree.setRootIndex(self.file_system.index(directory))
 
 
 if __name__ == '__main__':
