f2deac2b7c43e549139a555260f045e4d59bfc03
Casper Schippers <c.f.schippers@tue.nl>
2019-08-30 15:07:10 +0200

    Implemented pyqtgraph for viewing data

---
+++ NanoAccess_Log_Viewer/log_reader.py
@@ -0,0 +1,24 @@
+import pandas
+
+
+# def timeparser(t):
+#     return pandas.Timedelta(t, '%H:%M:%S')
+
+TD0 = pandas.Timedelta(0)
+TD1 = pandas.Timedelta("1D")
+
+
+def readLogFile(filename):
+    data = pandas.read_csv(
+        filename,
+        parse_dates=['Time'],
+        # date_parser=timeparser,
+        delim_whitespace=True,
+    )
+    data["Time"] = data["Time"] - data["Time"][0]
+
+    while any(data["Time"] < TD0):
+        data.loc[data["Time"] < TD0, "Time"] += TD1
+
+    return data
+
--- NanoAccess_Log_Viewer/main.py
+++ NanoAccess_Log_Viewer/main.py
@@ -1,13 +1,16 @@
 import sys
 from PyQt5 import QtWidgets, QtCore, QtGui
 import pyqtgraph as pg
-
+from log_reader import readLogFile
 
 pg.setConfigOption('background', 'w')
 pg.setConfigOption('foreground', 'k')
 
 
 class Viewer(QtWidgets.QMainWindow):
+
+    Data = []
+
     def __init__(self):
         super().__init__()
 
@@ -30,6 +33,13 @@ class Viewer(QtWidgets.QMainWindow):
         open_file.setShortcut("Ctrl+O")
         open_file.triggered.connect(self.openFiles)
 
+        open_file = file_menu.addAction("&Close")
+        open_file.setStatusTip("Close application")
+        open_file.setIcon(self.style().standardIcon(
+            QtWidgets.QStyle.SP_DialogCloseButton))
+        open_file.setShortcut("Ctrl+W")
+        open_file.triggered.connect(self.close)
+
     def initStatusBar(self):
         self.statusbar = self.statusBar()
         self.statusbar.showMessage("Ready")
@@ -40,18 +50,40 @@ class Viewer(QtWidgets.QMainWindow):
 
         central_layout = QtWidgets.QGridLayout(central_widget)
 
-        self.plot_widget = pg.PlotWidget()
+        # date_axis = pg.graphicsItems.DateAxisItem.DateAxisItem(
+        #     orientation='bottom')
+        self.plot_widget = pg.PlotWidget(
+            # axisItems={"bottom": date_axis}
+        )
         plotItem = self.plot_widget.getPlotItem()
         plotItem.showAxis('right')
         plotItem.showAxis('top')
         central_layout.addWidget(self.plot_widget, 0, 1, 1, 7)
 
-    def openFiles(self):
-        self.selectFiles()
+    def openFiles(self, *, filenames=None):
+        if filenames is None:
+            self.selectFiles()
+        else:
+            self.filenames = filenames
+
+        self.log_data = [readLogFile(file) for file in self.filenames]
+
+        self.plotData()
 
     def selectFiles(self):
         self.filenames, _ = QtWidgets.QFileDialog.getOpenFileNames(
             self, 'Open File')
+
+    def plotData(self):
+        # plotitem = self.plot_widget.getPlotItem()
+
+        self.plot_widget.clear()
+        for i, data in enumerate(self.log_data):
+            self.plot_widget.plot(
+                data["Time"].dt.total_seconds() / 60,
+                data["GAUGE-G41.p[mBar]"],
+                pen=(i, len(self.log_data)),
+            )
 
 
 if __name__ == '__main__':
